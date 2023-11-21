6d46bc29f9f8d4b2c1ebea087eff84cf3aca658e
Mateo Garcia <mateogarcia1660@gmail.com>
2023-11-21 19:56:05 -0300

    0.06

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,38 +1,170 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
-export const CartContext = createContext({
-    cart: []
-})
+export const CartContext = createContext()
 
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
+    const [totalProductsAdded, setTotalProductsAdded] = useState(0)
+    const [totalToPay, setTotalToPay] = useState(0)
 
-    console.log(cart)
+    useEffect(() => {
+        updateTotalProductsAdded()
+        updateTotalToPay()
+    }, [cart]) //eslint-disable-line
 
-    const addItem = (item, quantity) => {
-        if(!isInCart(item.id)) {
-            setCart(prev => [...prev, {...item, quantity}])
+    const addItem = (productToAdd) => {
+        if(!isInCart(productToAdd.id)) {
+            setCart([...cart, productToAdd])
         } else {
-            console.error('El producto ya fue agregado')
+            const newProducts = cart.map(prod => {
+                if(prod.id === productToAdd.id) {
+                    const newProduct = {
+                        ...prod,
+                        quantity: productToAdd.quantity
+                    }
+                    return newProduct
+                } else {
+                    return prod
+                }
+            })
+            setCart(newProducts)
         }
     }
 
-    const removeItem = (itemId) => {
-        const cartUpdated = cart.filter(prod => prod.id !== itemId)
-        setCart(cartUpdated)
+    const updateTotalProductsAdded = () => {
+        let count = 0
+        cart.forEach(prod => {
+            count += isNaN(prod.quantity) ? 0 : prod.quantity;
+        })
+
+        setTotalProductsAdded(count)
+    }
+
+    const updateTotalToPay = () => {
+        let total = 0
+        cart.forEach(prod => {
+            total += prod.quantity * prod.price
+        })
+        
+        setTotalToPay(total)
+    }
+    
+    const isInCart = (id) => {
+        return cart.some(p => p.id === id )
     }
 
     const clearCart = () => {
         setCart([])
     }
 
-    const isInCart = (itemId) => {
-        return cart.some(prod => prod.id === itemId)
+    const removeItem = (id) => {
+        const products = cart.filter(prod => prod.id !== id )
+        setCart(products)
     }
 
-    return (
-        <CartContext.Provider value={{ cart, addItem, removeItem, clearCart }}>
-            { children }
+    const getProductQuantity = (id) => {
+        return cart.find(prod => prod.id === id)?.quantity
+    }
+
+
+    return(
+        <CartContext.Provider value={{
+            cart,
+            totalProductsAdded,
+            totalToPay,
+            addItem,
+            isInCart,
+            clearCart,
+            removeItem,
+            getProductQuantity,
+
+        }}>
+            {children}
         </CartContext.Provider>
     )
 }
+
+export const useCart = () => {
+    return useContext(CartContext)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createContext, useState } from "react";
+
+// export const CartContext = createContext({
+//     cart: []
+// })
+
+// export const CartProvider = ({ children }) => {
+//     const [cart, setCart] = useState([])
+
+//     console.log(cart)
+
+//     const addItem = (item, quantity) => {
+//         if(!isInCart(item.id)) {
+//             setCart(prev => [...prev, {...item, quantity}])
+//         } else {
+//             console.error('El producto ya fue agregado')
+//         }
+//     }
+
+//     const removeItem = (itemId) => {
+//         const cartUpdated = cart.filter(prod => prod.id !== itemId)
+//         setCart(cartUpdated)
+//     }
+
+//     const clearCart = () => {
+//         setCart([])
+//     }
+
+//     const isInCart = (itemId) => {
+//         return cart.some(prod => prod.id === itemId)
+//     }
+
+//     return (
+//         <CartContext.Provider value={{ cart, addItem, removeItem, clearCart }}>
+//             { children }
+//         </CartContext.Provider>
+//     )
+// }
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,16 +3,55 @@ import cart from './assets/cart.png'
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
-
 const CartWidget = () => {
-    const { totalQuantity } = useContext(CartContext)
+    const { totalProductsAdded } = useContext(CartContext)
+
+    console.log('Total de productos agregados:', totalProductsAdded);
     
     return (
-        <Link className='i' to='/cart' style={{ display: totalQuantity > 0 ? 'block' : 'none'}}>
+        <Link className='i' to='/cart' style={{ display: totalProductsAdded > 0 ? 'block' : 'none'}}>
             <img className='i' src={cart} alt='cart-widget'/>
         </Link>
-        
+
     )
 }
 
+
+
 export default CartWidget
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { CartContext } from '../../Context/CartContext'
+// import cart from './assets/cart.png'
+// import { useContext } from 'react'
+// import { Link } from 'react-router-dom'
+
+
+// const CartWidget = () => {
+//     const { totalQuantity } = useContext(CartContext)
+    
+//     return (
+//         <Link className='i' to='/cart' style={{ display: totalQuantity > 0 ? 'block' : 'none'}}>
+//             <img className='i' src={cart} alt='cart-widget'/>
+//         </Link>
+        
+//     )
+// }
+
+// export default CartWidget
